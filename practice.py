3f3ea386d045f7be4a45e3de999dd025f2b4eb6a
8374742702 <154434270+8374742702@users.noreply.github.com>
2023-12-21 17:43:48 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,67 @@
+'''Int number
+enter the marks
+updated marks'''
+marks = 35
+print("the marks;",(type(marks)),"the address of marks;",(id(marks)))
+Marks = marks+15
+print(Marks)
+print("the updatedMarks;",(type(Marks)),"the address of updated Marks;",(id(Marks)))
+'''float....
+ enter the marks
+  updated marks'''
+marks = 34.5
+print("the marks;",(type(marks)),"the address of marks;",(id(marks)))
+Mark = marks+15.3
+print(Mark)
+print("the updated Mark;",(type(Mark)),"the address of updated Mark;",(id(Mark)))
+'''complex...
+enter the marks
+ updated marks'''
+marks = 34.50j
+print("the marks;",(type(marks)),"the address of marks;",(id(marks)))
+Mar = marks+26.50j
+print(Marks)
+print("the updated Marks;",(type(Marks)),"the address of  updated Marks;",(id(Marks)))
+'''string....
+enter the name
+ updated name'''
+name = 'arun'
+print("the marks;",(type(name)),"the address of name;",(id(name)))
+Name = name+'kumar'
+print(Name)
+print("the updated Name;",(type(Name)), "the address of updated Name;",(id(Name)))
+'''boolean....
+enter the value
+ updated value'''
+value = True
+print("the value;",(type(value)),"the address of value;",(id(value)))
+Value = False
+print("the Value;",(type(Value)),"the address of Value;",(id(Value)))
+# Created by arun
+# Area of radius
+# DMY;20-12-2023
+radius = (int(input("enter the circle of radius")))
+result =3.142*radius*radius
+print(result)
+# Created by arun
+# Area of triangle
+# DMY;20-12-2023
+base = (int(input("enter the triangle of base")))
+height = (int(input("enter the triangle of height")))
+area = base*height/2
+print(area)
+# Created by arun
+# Area of rectangle
+# DMY;20-12-2023
+length = (int(input("enter the length of rectangle")))
+width = (int(input("enter the width of rectangle")))
+area = length*width
+print(area)
+age = 35
+print("the age;",(type(age)),"the address of age;",(id(age)))
+Age = age+50
+print("the Age;",(type(Age)),"the address of Age;",(id(Age)))
+Age: object
+_Age = 40
+print("the _Age;",(type(_Age)),"the adress of -age;",(id(_Age)))
+
