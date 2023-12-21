4cde446a5f91ff150979ae384cfd9e4a50d28911
8374742702 <154434270+8374742702@users.noreply.github.com>
2023-12-21 17:44:50 +0530

    Add files via upload

---
+++ practce1.py
@@ -0,0 +1,386 @@
+# created by arun
+# dmy;21-12-2023
+# DATA TYPES
+#  int of boolean
+# salary = 50000
+# isCredited = False
+# bSalary = bool(salary)
+# print("the salary is credited;",isCredited,"check",bool(salary))
+# updatedSalary = bSalary+1000
+# print(updatedSalary)
+# bUpdatedSalary = bool(updatedSalary)
+# print("the updatedSalary;",isCredited,"check",bool(bUpdatedSalary),"the updatedSalary;",type(bUpdatedSalary))
+# print("it is downCasting")
+#
+# # int to float
+#
+#
+# salary = 50000
+# isCredited = False
+# bSalary = float(salary)
+# print("the salary is credited;",isCredited,"check",float(salary))
+# updatedSalary = bSalary+1000
+# print(updatedSalary)
+# bUpdatedSalary = float(updatedSalary)
+# print("the updatedSalary;",isCredited,"check",float(bUpdatedSalary),"the updatedSalary;",type(bUpdatedSalary))
+# print("it is upperCasting")
+#
+# # int to string
+#
+# salary = 50000
+# aSalary = str(salary)
+# print("the salary is credited;","check",str(salary),type(aSalary))
+# updatedSalary = aSalary
+# print(updatedSalary)
+# aUpdatedSalary = str(updatedSalary)
+# print("the updatedSalary;",isCredited,"check",str(aUpdatedSalary),"the updatedSalary;",type(aUpdatedSalary))
+# print("it is upperCasting")
+#
+# # int of complex
+#
+# salary = 50000.0j
+# aSalary = complex(salary)
+# print("the salary is credited;","check",complex(salary),type(aSalary))
+# updatedSalary = aSalary
+# print(updatedSalary)
+# aUpdatedSalary = complex(updatedSalary)
+# print("the updatedSalary;",isCredited,"check",complex(aUpdatedSalary),"the updatedSalary;",type(aUpdatedSalary))
+# print("it is uppCasting")
+#
+# # float of boolean
+#
+# cost = 50000.04
+# isCredited = False
+# bCost = bool(cost)
+# print("this is cost;",isCredited,"check",bool(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = bool(updatedCost)
+# print("the updatedCost;",isCredited,"check",bool(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is downCasting")
+#
+# # float of int
+#
+# cost = 50000.05
+# bCost = int(cost)
+# print("this is cost;",isCredited,"check",int(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = int(updatedCost)
+# print("the updatedCost;",isCredited,"check",int(bUpdatedCost),"the updatedcost;",type(bUpdatedCost))
+# print("it is downCasting")
+#
+# # float of string
+#
+# cost = 50000.04
+# bCost = str(cost)
+# print("this is cost;",isCredited,"check",str(cost))
+# updatedCost = bCost+"1000"
+# print(updatedCost)
+# bUpdatedCost = str(updatedCost)
+# print("the updatedCost;",isCredited,"check",str(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is upcasting")
+#
+# # float of complex
+#
+# cost = 50000.0
+# bCost = complex(cost)
+# print("this is cost;",isCredited,"check",complex(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = complex(updatedCost)
+# print("the updatedCost;",isCredited,"check",complex(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is upcasting")
+#
+# # str of boolean
+#
+# cost = "50000"
+# isCredited = False
+# bCost = bool(cost)
+# print("this is cost;",isCredited,"check",bool(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = bool(updatedCost)
+# print("the updatedCost;",isCredited,"check",bool(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is downCasting")
+#
+# # str of  int
+#
+# cost = "50000"
+# bCost = int(cost)
+# print("this is cost;",isCredited,"check",int(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = int(updatedCost)
+# print("the updatedCost;",isCredited,"check",int(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is downCasting")
+#
+# # str of float
+#
+# cost = "50000"
+# bCost = float(cost)
+# print("this is cost;",isCredited,"check",float(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = float(updatedCost)
+# print("the updatedCost;",isCredited,"check",float(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is down casting")
+#
+# # str of complex
+#
+# cost = "50000"
+# bCost = complex(cost)
+# print("this is cost;",isCredited,"check",complex(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = complex(updatedCost)
+# print("the updatedCost;",isCredited,"check",complex(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is upcasting")
+#
+# # complex of boolean
+#
+# cost = 50000.0j
+# isCredited = False
+# bCost = bool(cost)
+# print("this is cost;",isCredited,"check",bool(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = bool(updatedCost)
+# print("the updatedCost;",isCredited,"check",bool(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# # print("it is downCasting
+# # # complex of int
+# # bCost = int(cost)
+# # print("this is cost;",isCredited,"check",int(cost))
+# # updatedCost = bCost+1000
+# # print(updatedCost)
+# # bUpdatedCost = int(updatedCost)
+# # print("the updatedCost;",isCredited,"check",int(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# # # print("it is downCasting"
+# #
+# # # complex of float
+# # cost = 50000.9j
+# # bCost = float(cost)
+# # print("this is cost;",isCredited,"check",float(cost))
+# # updatedCost = bCost+1000
+# # print(updatedCost)
+# # bUpdatedCost = float(updatedCost)
+# # print("the updatedCost;",isCredited,"check",float(bUpdatedCost),"the updatedCost;",float(bUpdatedCost))
+# # # print("it is downCasting"
+#
+# # # complex of str
+#
+# cost = 50000.0j
+# bCost = str(cost)
+# print("this is cost;",str(cost))
+# updatedCost = bCost+"1000"
+# print(updatedCost)
+# bUpdatedCost = str(updatedCost)
+# print("the updatedCost;",isCredited,"check",str(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# # print("it is down casting")
+#
+# # boolean of int
+#
+# cost = True
+# isCredited = False
+# bCost = bool(cost)
+# print("this is cost;",isCredited,"check",bool(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = bool(updatedCost)
+# print("the updatedCost;",isCredited,"check",bool(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+#
+# # boolean of float
+#
+# cost = True
+# bCost = float(cost)
+# print("this is cost;",isCredited,"check",float(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = float(updatedCost)
+# print("the updatedCost;",isCredited,"check",float(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is up casting")
+#
+# # boolean of str
+#
+# cost = True
+# bCost = str(cost)
+# print("this is cost;",isCredited,"check",str(cost))
+# updatedCost = bCost+"1000"
+# print(updatedCost)
+# bUpdatedCost = str(updatedCost)
+# print("the updatedCost;",isCredited,"check",str(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is upcasting")
+#
+# # boolean of complex
+#
+# cost = True
+# bCost = complex(cost)
+# print("this is cost;",isCredited,"check",complex(cost))
+# updatedCost = bCost+1000
+# print(updatedCost)
+# bUpdatedCost = complex(updatedCost)
+# print("the updatedCost;",isCredited,"check",complex(bUpdatedCost),"the updatedCost;",type(bUpdatedCost))
+# print("it is upcasting")
+
+# created by arun
+# dmy:21-12-2023
+
+# TOPIC: Arithmatic Operators
+
+
+# i_num1 = 50
+# i_num2 = 40
+# i_num3 = 71
+# result = i_num1+i_num2
+# print("the sum of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num2))
+# result = i_num1*i_num3
+# print("the multiplication of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num3))
+# result = i_num3/i_num2
+# print("the division of two numbers;",result)
+# print("the address of num1 is ;",id(i_num3),"/n the address of num2 is",id(i_num2))
+# result = i_num3-i_num1
+# print("the sub of two numbers;",result)
+# print("the address of num1 is ;",id(i_num3),"/n the address of num2 is",id(i_num1))
+# result = i_num1//i_num2
+# print("the floor of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num2))
+# result = i_num2%i_num2
+# print("the mod of two numbers;",result)
+# print("the address of num1 is ;",id(i_num2),"/n the address of num2 is",id(i_num2))
+# result = i_num1**i_num2
+# print("the exponent of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num2))
+
+# float
+
+# f_num1 = 50.7
+# f_num2 = 40.8
+# f_num3 = 71.2
+# result = f_num1+f_num2
+# print("the sum of two numbers;",result)
+# print("the address of num1 is ;",id(f_num1),"/n the address of num2 is",id(f_num2))
+# result = f_num1*f_num3
+# print("the multiplication of two numbers;",result)
+# print("the address of num1 is ;",id(f_num1),"/n the address of num2 is",id(f_num3))
+# result = f_num3/f_num2
+# print("the division of two numbers;",result)
+# print("the address of num1 is ;",id(f_num3),"/n the address of num2 is",id(f_num2))
+# result = f_num3-f_num1
+# print("the sub of two numbers;",result)
+# print("the address of num1 is ;",id(f_num3),"/n the address of num2 is",id(f_num1))
+# result = f_num1//f_num2
+# print("the floor of two numbers;",result)
+# print("the address of num1 is ;",id(f_num1),"/n the address of num2 is",id(f_num2))
+# result = f_num2%f_num2
+# print("the mod of two numbers;",result)
+# print("the address of num1 is ;",id(f_num2),"/n the address of num2 is",id(f_num2))
+# result = f_num1**f_num2
+# print("the exponent of two numbers;",result)
+# print("the address of num1 is ;",id(f_num1),"/n the address of num2 is",id(f_num2))
+
+# complex
+
+# i_num1 = 50.04j
+# i_num2 = 40.34j
+# i_num3 = 71.78j
+# result = i_num1+i_num2
+# print("the sum of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num2))
+# result = i_num1*i_num3
+# print("the multiplication of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num3))
+# result = i_num3/i_num2
+# print("the division of two numbers;",result)
+# print("the address of num1 is ;",id(i_num3),"/n the address of num2 is",id(i_num2))
+# result = i_num3-i_num1
+# print("the sub of two numbers;",result)
+# print("the address of num1 is ;",id(i_num3),"/n the address of num2 is",id(i_num1))
+# result = i_num1**3
+# print("the exponent of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num3))
+# result = i_num1//i_num2
+# print("the floor of two numbers;",result)
+# print("the address of num1 is ;",id(i_num1),"/n the address of num2 is",id(i_num2))
+# result = i_num2%i_num2
+# print("the mod of two numbers;",result)
+# print("the address of num1 is ;",id(i_num2),"/n the address of num2 is",id(i_num2))
+
+
+# created by arun
+# dmy:21-12-2023
+# TOPIC:Assignment Operators
+
+#int
+
+num1 = 40
+num1 += 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 -= 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 *= 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 %= 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 /= 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 //= 10
+print("the result:", num1, type(num1), id(num1))
+num1 = 40
+num1 **= 10
+print("the result:", num1, type(num1), id(num1))
+
+# float
+
+a = 50.9
+a += 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a -= 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a %= 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a *= 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a /= 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a //= 10
+print("the result:", a, type(a), id(a))
+a = 50.9
+a **= 10
+print("the result:", a, type(a), id(a))
+
+# complex
+
+a = 40.9j
+a += 30
+print("the result:", a, type(a), id(a))
+a = 40.9j
+a -= 30
+print("the result:", a, type(a), id(a))
+# a = 40.9j
+# a %= 30
+# print("the result:", a, type(a), id(a))
+a = 40.9j
+a *= 30
+print("the result:", a, type(a), id(a))
+a = 40.9j
+a **= 30
+print("the result:", a, type(a), id(a))
+a = 40.9j
+a /= 30
+print("the result:", a, type(a), id(a))
+a = 40.9j
+# a //= 30
+# print("the result:", a, type(a), id(a))
+
